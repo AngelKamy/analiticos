17d4f1f10f15de7ddaecdd5a24522229b167a241
Angel Vasquez <ivanvaq3@gmail.com>
2024-03-25 14:51:38 -0600

    chore: Se separaron los JSON del código principal para una mejor legilibilidad

---
+++ js/detalles.js
@@ -0,0 +1,53 @@
+export const detalles = {
+    'Niño': [
+      "1. Incorporación a protocolos de atención preventiva",
+      "2. Medición de peso y talla < 5 años",
+      "3. Medición de peso y talla 5 a 9 años",
+      "4. Prevención de anemia < 1 año",
+      "5. Aplicación tópica de flúor en niños de 4 años",
+      "6. Defectos visuales"
+    ],
+    'Adolescente': [
+      "1. Incorporación a protocolos de atención preventiva",
+      "2. Medición de peso y talla",
+      "3. Toxoide tetánico diftérico",
+      "4. Hepatitis B",
+      "5. Sarampión / rubeola",
+      "6. Entrega informada de condones",
+      "7. Defectos visuales"
+    ],
+    'Mujer': [
+      "1. Incorporación a protocolos de atención preventiva",
+      "2. Medición de peso y talla",
+      "3. CACU 25 a 69 años",
+      "4. Exploración clínica de mama",
+      "5. Mastografía",
+      "6. Diabetes mellitus",
+      "7. Hipertensión arterial",
+      "8. Tuberculosis pulmonar"
+    ],
+    'Hombre': [
+      "1. Incorporación a protocolos de atención preventiva",
+      "2. Medición de peso y talla",
+      "3. Diabetes mellitus",
+      "4. Hipertensión arterial",
+      "5. Tuberculosis pulmonar"
+    ],
+    'Adulto Mayor': [
+      "1. Incorporación a protocolos de atención preventiva",
+      "2. Medición de peso y talla",
+      "3. Antineumocóccica",
+      "4. Anti influenza",
+      "5. Diabetes mellitus",
+      "6. Hipertensión arterial",
+      "7. Tuberculosis pulmonar"
+    ],
+    'Toda la Unidad': [
+      "1. Niño",
+      "2. Adolescente",
+      "3. Mujer",
+      "4. Hombre",
+      "5. Adulto Mayor"
+    ]
+
+  };
